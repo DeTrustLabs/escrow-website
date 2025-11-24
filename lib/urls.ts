c08dc68d8064d41d906a888c9c168af2cf10e7a6
b3hr4d <behradmusiclove@yahoo.com>
2025-11-24 18:02:43 +0300

    fix: Update APP_URL to use the correct production URL

--- lib/urls.ts
+++ lib/urls.ts
@@ -2,8 +2,7 @@
 // Adjust NEXT_PUBLIC_APP_URL in a .env.local file to point to staging/production dapp instance if needed.
 
 export const APP_URL =
-  process.env.NEXT_PUBLIC_APP_URL ||
-  "https://qhsea-iaaaa-aaaaj-qa6kq-cai.icp0.io"
+  process.env.NEXT_PUBLIC_APP_URL || "https://app.escrow-protocol.com"
 export const SITE_URL =
   process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
 
